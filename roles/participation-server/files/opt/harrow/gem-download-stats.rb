0f5248c160db0b455b83888186edfda63fe79672
Lee Hambley <leehambley@harrow.io>
2016-05-06 20:33:49 +0200

    CGI-enable download statistics

--- roles/participation-server/files/opt/harrow/gem-download-stats.rb
+++ roles/participation-server/files/opt/harrow/gem-download-stats.rb
@@ -1,39 +1,27 @@
 #!/usr/bin/env ruby
 
 require 'csv'
+require 'cgi'
 require 'pstore'
+
+cgi = CGI.new
+gem = cgi["gem"]
 
 s = PStore.new('stats.pstore')
 s.transaction(true) do
-
-  gems = s[s.roots.last].keys
-
-  # gems.each do |gem| most_versions =
-  # end
-
-  gems.each do |gem|
-
-    puts "# #{gem}"
-    versions = s[s.roots.last][gem].keys
-
-    csv_string = CSV.generate(headers: true, force_quotes: true) do |csv|
-
+  versions = s[s.roots.last][gem].keys
+  cgi.out("status" => "OK", "type" => "text/csv", "connection" => "close") do
+    CSV.generate(headers: true, force_quotes: true) do |csv|
       csv << ["DateTime"] + versions.collect { |v| "v#{v}" }
-
       s.roots.each do |time_bucket|
-
         row = [time_bucket.utc]
         versions.each do |v|
           row << s[time_bucket][gem][v]
         end
-
         csv << row
       end
-
     end
-    puts csv_string
   end
-
-
 end
+
 
